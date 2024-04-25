907e520597a5e708c5415c39bdf67abff8f9fd32
Nadir <79022605+mustafafiliz@users.noreply.github.com>
2024-04-25 13:56:50 +0300

    fix: static constants

--- src/mocks/static.js
+++ src/mocks/static.js
@@ -1,19 +1,5 @@
-export const durationUnits = ['gün', 'hafta', 'ay', 'yıl']
+export const durationUnits = ["gün", "hafta", "ay", "yıl"];
 
-export const currencies = ['TRY']
+export const currencies = ["TRY"];
 
-export const packageTypes = ['Muayene', 'Kontrol']
-
-export const calculateEndDate = (startDate, incrementType, incrementValue) => {
-  const date = new Date(startDate)
-
-  if (incrementType === 'day') {
-    date.setDate(date.getDate() + incrementValue)
-  } else if (incrementType === 'week') {
-    date.setDate(date.getDate() + 7 * incrementValue)
-  } else if (incrementType === 'month') {
-    date.setMonth(date.getMonth() + incrementValue)
-  }
-
-  return date
-}
+export const packageTypes = ["Muayene", "Kontrol"];
